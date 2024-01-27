85113c0e7c520c69cfa32c2fb86e5858dc229265
unknown <mtharwat@onesolutionc.com>
2024-01-27 19:04:09 +0200

    update colors for be dynamic and random generation

--- src/Components/Charts/pieChart.js
+++ src/Components/Charts/pieChart.js
@@ -19,14 +19,35 @@ const PieChart = ({ data }) => {
         },
       },
     };
+    // Function to generate an array of colors
+    const generateColors = (length) => {
+      const colors = [];
+      for (let i = 0; i < length; i++) {
+        // Generate a random color or use a predefined color array
+        // You can modify this logic based on your requirements
+        const color = getRandomColor();
+        colors.push(color);
+      }
+      return colors;
+    };
 
+    // Function to generate a random color
+    const getRandomColor = () => {
+      const letters = '0123456789ABCDEF';
+      let color = '#';
+      for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 10)];
+      }
+      return color;
+};
+    const colors = generateColors(data.length);
     const myChart = new Chart(chartRef.current, {
       type: 'pie',
       data: {
         labels: data.map(item => item.month),
         datasets: [{
           data: data.map(item => parseFloat(item.profit)),
-          backgroundColor: ['red', 'blue', 'green', 'orange', 'purple', 'yellow'],
+          backgroundColor: colors,
         }],
       },
       options: chartOptions,
